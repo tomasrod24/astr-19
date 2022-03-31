4c1469be51845d9c8c7b22ad60258b3b37589b8f
tomasrod24 <102757473+tomasrod24@users.noreply.github.com>
2022-03-31 11:02:03 -0700

    Add files via upload

---
+++ inClassProgram.py
@@ -0,0 +1,14 @@
+#this program will write hello world
+
+#this function tells the computer to print hello world
+
+def PrintHelloWorld():
+	print("Hello World!")
+
+#this defines our main() function for our programs
+def main():
+	PrintHelloWorld()
+
+#when we run the program this excutes first 
+if __name__ == '__main__':
+	main()
