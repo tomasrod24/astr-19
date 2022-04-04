3b408c58419c3bcca282118d32dd9ff25baea2d9
tomasrod24 <102757473+tomasrod24@users.noreply.github.com>
2022-04-04 14:01:28 -0700

    Add files via upload

---
+++ Session1PromptTomasR.py
@@ -0,0 +1,12 @@
+#this program will print out my full name
+#in addition it will also state my prefered pronouns and a few sentences about my favorite food and movie
+
+t = 'Tomas Rodriguez' #saving my name as a string as variable t
+print(t)              #printing out variable t
+
+p = 'My preferred pronouns are he/his' #just as before I am saving my preffered pronouns as a string in variable p
+print(p)                               #again just as before I am calling the print function to print out variable p
+
+#for these two lines I am using the print function and quotations instead of saving my strings as a variable
+print("My favorite movie is the new batman movie that just came out this year.") 
+print("Now my favorite food is a bit of a hard choice but I would have to say a classic in pizza.")
