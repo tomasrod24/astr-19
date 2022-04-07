d167957a06cb09a8febfb763f3ef4d012389f1aa
tomasrod24 <102757473+tomasrod24@users.noreply.github.com>
2022-04-06 19:46:51 -0700

    Add files via upload

---
+++ TomasRodSession2Prompt.py
@@ -0,0 +1,17 @@
+'''
+this program  prints the sum of two floating point numbers, 
+the difference between two integers, 
+and the product of a floating point number and an integer
+'''
+x = 7.0 #floating numbers are declared with a .0 after the intial number
+y = 5.0
+z = 6
+
+print("x + y = ", x+y)
+print(type(x+y)) #the type() function returns the data type of the answer
+
+print("x - y = ", x-y)
+print(type(x-y))
+
+print("x * z = ", x*z)
+print(type(x*z))
